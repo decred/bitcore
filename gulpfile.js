b2cd88dab2a77da108953b499e80227f916f0b9e
life <lifephp@gmail.com>
2019-01-11 14:21:36 +0800

    fix https://github.com/decred/bitcore/issues/19

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
-
-
 var bitcoreTasks = require('bitcore-build');
+var gulp = require('gulp');
+var tasks = bitcoreTasks('lib');
 
-bitcoreTasks('lib');
+for (var task in tasks) {
+    gulp.task(task, tasks[task])
+}
